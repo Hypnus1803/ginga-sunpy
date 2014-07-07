e391ba513484e164a32a7d21301c725c6f76d04b
Stuart Mumford <stuart@mumford.me.uk>
2014-07-07 18:43:18 +0100

    tweak setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 from setuptools import setup, find_packages
 
 setup(
-      name='sunpy-ginga',
+      name='sunpy_ginga',
       version='0.1dev',
       packages=find_packages(),
       scripts=glob.glob('./scripts/*')
